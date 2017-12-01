a52a61563587fa707b36d57ffd105c4b3d1bb2df
Olivier Cloux <olivier.cloux@epfl.ch>
2017-12-01 15:57:14 +0100

    A4

--- Assignment-3-handout/assignment3.c
+++ Assignment-3-handout/assignment3.c
@@ -11,6 +11,8 @@
 #include <stdlib.h>
 #include <omp.h>
 
+#define ERRCODE -1
+
 typedef struct node {
     int val;
     struct node *next;
@@ -21,7 +23,6 @@ typedef struct node {
 /*
  * This function initializes a new linked list.
  */
-
 void init_list(node_t **head, int val) {
     (*head) = malloc(sizeof(node_t));
     if(NULL == *head){
@@ -29,6 +30,7 @@ void init_list(node_t **head, int val) {
     }
     (*head)->val = val;
     (*head)->next = NULL;
+    //new node -> init its lock
     omp_init_lock(&((*head)->lock));
 }
 
@@ -36,17 +38,21 @@ void init_list(node_t **head, int val) {
  * This function prints all the elements of a given linked list.
  */
 void print_list(node_t *head) {
+    /*Need to lock an element, lock its next then unlock former.
+    * Ensures we don't run into strange modifications
+    */
     node_t *current = head;
     omp_set_lock(&(current->lock));
+
     while (current != NULL) {
-        printf("%d --> ", current->val);
+        printf("%d\n", current->val);
         if(current->next != NULL){
+            //don't need to lock after the last element
             omp_set_lock(&((current->next)->lock));
         }
         omp_unset_lock(&(current->lock));
         current = current->next;
     }
-    printf("NULL\n");
 }
 
 /*
@@ -54,6 +60,9 @@ void print_list(node_t *head) {
  */
 int count(node_t *head)
 {
+    /*Similar as print : lock an element, its next and unlock former.
+    * This will ensure consistency.
+    */
     node_t *current = head;
     omp_set_lock(&(current->lock));
     int count = 0;
@@ -61,8 +70,10 @@ int count(node_t *head)
     while (current != NULL) {
         count++;
         if(current->next != NULL){
+            //lock the next, but not after last element
             omp_set_lock(&((current->next)->lock));
         }
+        //once next is locked, you can unlock current
         omp_unset_lock(&(current->lock));
         current = current->next;
     }
@@ -74,35 +85,41 @@ int count(node_t *head)
  * This function appends a new given element to the end of a given linked list.
  */
 void append(node_t *head, int val) {
-    if(NULL == head || -1 == val){
+    if(NULL == head || ERRCODE == val){
         //avoids SEGFAULT and confusion between value and errcode.
         return;
     }
     node_t *current = head;
-
+    omp_set_lock(&(current->lock)); //start off by locking head
     while (NULL != current->next) {
+        //lock next element, and once it's acquired unlock current
+        omp_set_lock(&((current->next)->lock));
+        omp_unset_lock(&(current->lock));
         current = current->next;
     }
-    omp_set_lock(&(current->lock));
+    //at this point, only current is locked (aka the last element)
     current->next = malloc(sizeof(node_t));
     if(NULL == current->next){
+        //if malloc failed, unlock everything and return
+        omp_unset_lock(&(current->lock));
         return;
     }
+    //is malloc succeeded, init the new node;
     current->next->val = val;
     current->next->next = NULL;
-    omp_init_lock(&((current->next)->lock));
-    omp_unset_lock(&(current->lock));
+    omp_init_lock(&((current->next)->lock)); //new node -> init its lock
+    omp_unset_lock(&(current->lock)); //release former tail
 }
 
 /*
  * This function adds a new given element to the beginning of a given linked list.
  */
 void add_first(node_t **head, int val){
-    if(NULL == head || NULL == *head || val == -1){
+    if(NULL == head || NULL == *head || val == ERRCODE){
         return;
     }
 
-    //node preparation
+    //node preparation : malloc, ensuring it succeeded, setting val,...
     node_t *new_node;
     new_node = malloc(sizeof(node_t));
     if(NULL == new_node){
@@ -111,11 +128,23 @@ void add_first(node_t **head, int val){
     new_node->val = val;
     omp_init_lock(&(new_node->lock));
 
+
     //head update
     node_t* prev_head = *head;
     while(omp_test_lock(&(prev_head->lock))){
-        //iterate until you obtain the correct (and free) head
+        /*Unlikely but possible case : two add_first are trying to execute
+        * concurrently or head is being deleted/updated. We update head
+        * until lock is clear (node was updated) or a new head is given and
+        thus free of lock (head was modified), so we can lock it
+        */
         prev_head = *head;
+        if(prev_head == NULL){
+            /*if reason lock was not free is that the list was being deleted,
+            * don't proceed*/
+            omp_destroy_lock(&(new_node->lock));
+            free(new_node);
+            return;
+        }
     }
     new_node->next = prev_head;
     *head = new_node; //update head of list
@@ -123,13 +152,15 @@ void add_first(node_t **head, int val){
 }
 
 /*
- * This function inserts a new given element (different than -1) at the specified position of a given linked list.
- * It returns 0 on a successful insertion, and -1 if the list is not long enough.
+ * This function inserts a new given element (different than ERRCODE) at the specified position of a given linked list.
+ * It returns 0 on a successful insertion, and ERRCODE if the list is not long enough.
  */
 int insert(node_t **head, int val, int index) {
-    if(NULL == head || NULL == *head || -1 == val){
-        //avoid SEGFAULT and confusion between a value and the errcode -1
-        return -1;
+    if(NULL == head || NULL == *head || ERRCODE == val){
+        /*avoid SEGFAULT if head or *head is null
+        * and confusion between a value and the ERRCODE
+        */
+        return ERRCODE;
     }
     if (index == 0) {
         add_first(head, val);
@@ -140,7 +171,8 @@ int insert(node_t **head, int val, int index) {
     omp_set_lock(&(current->lock));
     for (int i = 0; i < index-1; i++) {
         if (current->next == NULL) {
-            return -1;
+            omp_unset_lock(&(current->lock));
+            return ERRCODE;
         }
         omp_set_lock(&((current->next)->lock));
         omp_unset_lock(&(current->lock));
@@ -151,7 +183,7 @@ int insert(node_t **head, int val, int index) {
     new_node = malloc(sizeof(node_t));
     if(NULL == new_node){
         //to avoid SEGFAULT
-        return -1;
+        return ERRCODE;
     }
     new_node->val = val;
     new_node->next = current->next;
@@ -166,10 +198,10 @@ int insert(node_t **head, int val, int index) {
  * The value of that element is returned (if list is not empty), and the element is removed.
  */
 int pop(node_t **head) {
-    int retval = -1;
+    int retval = ERRCODE;
     node_t* next_node = NULL;
     if (*head == NULL) {
-        return -1;
+        return ERRCODE;
     }
     node_t* prev_head = *head;
     while(omp_test_lock(&(prev_head->lock))){
@@ -181,23 +213,22 @@ int pop(node_t **head) {
     *head = next_node; //update head
     retval = prev_head->val;
     omp_unset_lock(&(prev_head->lock));
+    omp_destroy_lock(&(prev_head->lock));
     free(prev_head);
 
     return retval;
 }
 
-
 /*
  * This function removes the specified element of a given linked list
- * The value of that element is returned if the list is long enough; otherwise it returns -1
+ * The value of that element is returned if the list is long enough; otherwise it returns ERRCODE
  * Please note the index starts from 0
  */
-
 int remove_by_index(node_t **head, int index) {
     if (index == 0) {
         return pop(head);
     }
-    int retval = -1;
+    int retval = ERRCODE;
 
 
     if(head == NULL || *head == NULL){
@@ -210,14 +241,14 @@ int remove_by_index(node_t **head, int index) {
 
     for (int i = 0; i <= index-1; i++) {
         if (current->next == NULL) {
-            //error, we list is not long enough. unlock everything and return
+            //error, list is not long enough. unlock everything and return
             if(previous != NULL){
               //need to unlock previous, but only if
-              //lock exists (will after 1st iteration)
+              //lock exists (only useful if list only has head)
               omp_unset_lock(&(previous->lock));
             }
             omp_unset_lock(&(current->lock));
-            return -1;
+            return ERRCODE;
         }
         /*Lock next and unlock previous, before shifting*/
         omp_set_lock(&((current->next)->lock));
@@ -228,12 +259,16 @@ int remove_by_index(node_t **head, int index) {
         previous = current;
         current = current->next;
     }
-    /*At this point, previous and current are locked*/
-    // Current node has to be removed!
+
+    /*At this point, previous and current are locked
+    * Current node has to be removed!
+    */
     previous->next = current->next;
     retval = current->val;
     omp_unset_lock(&(current->lock));
+    omp_destroy_lock(&(current->lock));
     free(current);
+    //once current node has been deleted and freed, we can unlock previous
     omp_unset_lock(&(previous->lock));
 
     return retval;
@@ -241,53 +276,63 @@ int remove_by_index(node_t **head, int index) {
 
 /*
  * This function removes the specified element of a given linked list.
- * The value of that element is returned if the element is found; otherwise it returns -1.
+ * The value of that element is returned if the element is found; otherwise it returns ERRCODE.
  */
 int remove_by_value(node_t **head, int val) {
     node_t *previous, *current;
 
-    if (*head == NULL) {
-        return -1;
+    if (head == NULL || *head == NULL || val == ERRCODE) {
+        return ERRCODE;
     }
 
     if ((*head)->val == val) {
         return pop(head);
     }
+
     previous = *head;
-    omp_set_lock(&(previous->lock));
     current = (*head)->next;
+    omp_set_lock(&(previous->lock));
     omp_set_lock(&(current->lock));
+
     while (current != NULL) {
-        if (current->val == val) {
+        if (current->val == val) { //value found
             previous->next = current->next;
             //free the element and locks, now that the operation has been done
             omp_unset_lock(&(current->lock));
+            omp_destroy_lock(&(current->lock));
             free(current);
             omp_unset_lock(&(previous->lock));
             return val;
         }
+        if(current->next != NULL){
+            //lock next (if possible) before unlocking previous
+            omp_set_lock(&((current->next)->lock));
+        }
         omp_unset_lock(&(previous->lock));
         previous = current;
         current  = current->next;
-        omp_set_lock(&(current->lock));
     }
-    return -1;
+
+    /*If not found, unlock everything and return ERRCODE*/
+    omp_unset_lock(&(current->lock));
+    return ERRCODE;
 }
 
 /*
  * This function searched for a specified element in a given linked list.
- * The index of that element is returned if the element is found; otherwise it returns -1.
+ * The index of that element is returned if the element is found; otherwise it returns ERRCODE.
  */
 int search_by_value(node_t *head, int val) {
     node_t *current = head;
     int index = 0;
 
-    if (current == NULL) {
-        return -1;
+    if (current == NULL || val == ERRCODE) {
+        return ERRCODE;
     }
     omp_set_lock(&(current->lock));
     while (current) {
         if (current->val == val) {
+            //value was found, unlock and return
             omp_unset_lock(&(current->lock));
             return index;
         }
@@ -299,13 +344,15 @@ int search_by_value(node_t *head, int val) {
         index++;
     }
 
-    return -1;
+    return ERRCODE;
 }
 
 /*
  * This function deletes all element of a given linked list, and frees up all the allocated resources.
  */
 void delete_list(node_t **head) {
+    //please note the signature, that takes a node_t** and not a node_t*
+    
     if(head == NULL || *head == NULL){
       //probable cause : the list has already been deleted; don't proceed.
       return;
@@ -321,19 +368,14 @@ void delete_list(node_t **head) {
             omp_set_lock(&(next->lock));
         }
         omp_unset_lock(&(current->lock));
+        omp_destroy_lock(&(current->lock));
         free(current);
         current = next;
     }
 }
 
 int main(void) {
-    // omp_lock_t head_lock, tail_lock, tail_lock;
-    // omp_init_lock(head_lock);
-    // omp_init_lock(middle_lock);
-    // omp_init_lock(tail_lock);
 
-    omp_lock_t head_lock;
-    omp_init_lock(&head_lock);
     node_t *test_list;
     init_list(&test_list, 2);
 
@@ -342,18 +384,18 @@ int main(void) {
     append(test_list, 3);
     append(test_list, 4);
     append(test_list, 5);
-    print_list(test_list);
+
     printf("Count = %d\n", count(test_list));
-    printf("Removed index 2 : %d\n",remove_by_index(&test_list, 2));
+    remove_by_index(&test_list, 2);
     printf("Count = %d\n", count(test_list));
 
-    printf("Removed index 100 : %d\n",remove_by_index(&test_list, 100));
     printf("Search for 5 -> index = %d\n", search_by_value(test_list, 5));
-    printf("Remove value 5 : %d\n", remove_by_value(&test_list, 5));
+    remove_by_value(&test_list, 5);
     printf("Search for 5 -> index = %d\n", search_by_value(test_list, 5));
 
     print_list(test_list);
     delete_list(&test_list);
+
     return 0;
 }
 
--- Assignment-3-handout/assignmentarnaud.c
+++ Assignment-3-handout/assignmentarnaud.c
@@ -32,28 +32,44 @@ void init_list(node_t **head, int val) {
 
 /*
 * This function prints all the elements of a given linked list.
-* TODO Should be done by only 1 thread.
+here we need to be sure that the current node is not
+deleted when we print it
+
+in order to do so we lock the current print lock the next one
+then unlock the current to move forward
 */
 void print_list(node_t *head) {
   node_t *current = head;
-
+  omp_set_lock(&current->lock);
   while (current != NULL) {
     printf("%d\n", current->val);
+    omp_set_lock(&current->next->lock);
+    omp_unset_lock(&current->lock);
     current = current->next;
+
   }
 }
 
 /*
 * This function counts the elements of a given linked list and returns the counted number.
-* TODO could it be parrallelised ? sounds like a 1 threaded function as for print.
+* here we need to be sure that the count is correct so no add node or delete on
+the current one we are counting.
+
+we go through the list the same way as print list.
+
 */
 int count(node_t *head)
 {
   node_t *current = head;
   int count = 0;
-
+  //lock the head
+  omp_set_lock(&current->lock);
   while (current != NULL) {
     count++;
+    //lock the next one
+    omp_set_lock(&current->next->lock);
+    //unlock the current and move forward
+    omp_unset_lock(&current->lock);
     current = current->next;
   }
 
@@ -62,6 +78,13 @@ int count(node_t *head)
 
 /*
 * This function appends a new given element to the end of a given linked list.
+
+Here we need to be sure that the last element of the list is
+the last element of the list.
+
+in order to do so we go through the list locking ...
+and when we found the last one we block on it until
+
 */
 void append(node_t *head, int val) {
   node_t *current = head;
@@ -69,16 +92,16 @@ void append(node_t *head, int val) {
   //lock the head
   omp_set_lock(&current->lock);
   while (current->next != NULL) {
+    //lock the next one
+    omp_set_lock(&current->next->lock);
     //go to next
     current = current->next;
-    //lock it
-    omp_set_lock(&current->lock);
     //unlock previous only if you are able to lock the current
     omp_unset_lock(&previous->lock);
     //update previous
     previous = current;
   }
-
+  printf("I DID IT\n");
   current->next = malloc(sizeof(node_t));
   current->next->val = val;
   current->next->next = NULL;
@@ -97,17 +120,22 @@ void add_first(node_t **head, int val) {
   new_node->val = val;
   new_node->next = *head;
 
-  //cette ligne compile pas
-  //omp_set_lock(&head->lock);
+  //init and lock it !
+  omp_init_lock(&new_node->lock);
+  omp_set_lock(&new_node->lock);
+
+  //lock the head
+  omp_set_lock(&new_node->next->lock);
   *head = new_node;
-  //as head become new node head become new_node->next
-  // omp_unset_lock(&new_node->next->lock);
+
+  //unlock after set
+  omp_unset_lock(&new_node->next->lock);
+  omp_unset_lock(&new_node->lock);
 }
 
 /*
 * This function inserts a new given element at the specified position of a given linked list.
 * It returns 0 on a successful insertion, and -1 if the list is not long enough.
-* TODO Same as add_first : should be multithreaded !!!!
 */
 int insert(node_t **head, int val, int index) {
   if (index == 0) {
@@ -116,20 +144,30 @@ int insert(node_t **head, int val, int index) {
   }
 
   node_t * current = *head;
+  //lock the head
+  omp_set_lock(&current->lock);
 
   for (int i = 0; i < index-1; i++) {
     if (current->next == NULL) {
       return -1;
     }
+    //lock the next node
+    omp_set_lock(&current->next->lock);
+    //unlock the current if you locked the next
+    omp_unset_lock(&current->lock);
     current = current->next;
+
   }
 
   node_t *new_node;
   new_node = malloc(sizeof(node_t));
   new_node->val = val;
   new_node->next = current->next;
-  current->next = new_node;
+  omp_init_lock(&new_node->lock);
 
+  current->next = new_node;
+  //can free because we have finish the modification
+  omp_unset_lock(&current->lock);
   return 0;
 }
 
@@ -145,12 +183,18 @@ int pop(node_t **head) {
   if (*head == NULL) {
     return -1;
   }
+  //lock the new head to be sure it will not be delete by an other one
+  omp_set_lock(&(*head)->next->lock);
 
   next_node = (*head)->next;
   retval = (*head)->val;
+  //we have to destroy the lock !!!
+  omp_destroy_lock(&(*head)->lock);
   free(*head);
   *head = next_node;
 
+  //unlock the new head when finish
+  omp_unset_lock(&next_node->lock);
   return retval;
 }
 
@@ -170,18 +214,36 @@ int remove_by_index(node_t **head, int index) {
     node_t * current = *head;
     node_t * previous = NULL;
 
+    //lock the head
+    omp_set_lock(&current->lock);
+
     for (int i = 0; i <= index-1; i++) {
         if (current->next == NULL) {
             return -1;
+        }
+        //lock the next node
+        omp_set_lock(&current->next->lock);
+        //if it's done unlock the current
+        if(i != 0 && i != index-1){
+          omp_unset_lock(&previous->lock);
         }
         previous = current;
         current = current->next;
     }
 
     // Current node has to be removed!
+    /*
+    there is no need to lock the current->next because
+    if an other thread attempt to delete it, the thread
+    would first lock the previous which are our current
+    so we would not be able to continue. We have to wait
+    for it to delete and replace it.
+    */
     previous->next = current->next;
     retval = current->val;
+    omp_destroy_lock(&current->lock);
     free(current);
+    omp_unset_lock(&previous->lock);
 
     return retval;
 }
@@ -204,13 +266,23 @@ int remove_by_value(node_t **head, int val) {
 
   previous = *head;
   current = (*head)->next;
+
+  //lock head and next
+  omp_set_lock(&previous->lock);
+  omp_set_lock(&current->lock);
+
   while (current) {
     if (current->val == val) {
       previous->next = current->next;
+      omp_destroy_lock(&current->lock);
       free(current);
       return val;
     }
 
+    //lock next
+    omp_set_lock(&current->next->lock);
+    //if ok unlock previous
+    omp_unset_lock(&previous->lock);
     previous = current;
     current  = current->next;
   }
@@ -229,11 +301,17 @@ int search_by_value(node_t *head, int val) {
     return -1;
   }
 
+  //lock the head
+  omp_set_lock(&current->lock);
+
   while (current) {
     if (current->val == val) {
       return index;
     }
-
+    //lock the next one
+    omp_set_lock(&current->next->lock);
+    //if ok unlock the current and jump
+    omp_unset_lock(&current->lock);
     current  = current->next;
     index++;
   }
@@ -247,23 +325,27 @@ int search_by_value(node_t *head, int val) {
 void delete_list(node_t *head) {
   node_t *current = head;
   node_t *next;
-
+  omp_set_lock(&current->lock);
   while (current) {
+    //lock the next one before continue
+    omp_set_lock(&current->next->lock);
     next = current->next;
+    //destroy before free
+    omp_destroy_lock(&current->lock);
     free(current);
     current = next;
   }
 }
 
 int main(void) {
-  printf("bonjour\n");
+
   node_t *test_list;
-  printf("je fonctionne\n");
+
   init_list(&test_list, 2);
-  printf("2\n");
   add_first(&test_list, 1);
-  printf("3\n");
+
   append(test_list, 3);
+
   append(test_list, 4);
   append(test_list, 5);
 
